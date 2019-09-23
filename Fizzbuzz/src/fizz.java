7b0ba9f83c793dbe2daa1dddb2131d11e4d4df31
kaykay89 <kaylalouis@hotmail.com>
2019-09-23 10:05:07 -0400

    Petit programme Fizzbuzz crée

---
+++ Fizzbuzz/src/fizz.java
@@ -0,0 +1,22 @@
+
+public class fizz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int nombre = 1;
+		while (nombre <= 200){
+			if (nombre % 3 == 0 && nombre % 5 == 0){
+				System.out.print("FizzBuzz \n");
+			}else if (nombre % 3 == 0){
+				System.out.print("Fizz \n");
+			}else if (nombre % 5 == 0){
+				System.out.print("Buzz \n");
+			}else{
+				System.out.print(nombre + " \n");
+			}
+			nombre++;
+		
+		}
+	}
+
+}
